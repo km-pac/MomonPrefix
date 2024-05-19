35fb43aae39906151e590f0d77c17de2a9eddb4c
KylePac <147895229+km-pac@users.noreply.github.com>
2024-05-19 12:29:32 +0800

    Final filter applied

--- testpingmomon.py
+++ testpingmomon.py
@@ -54,5 +54,6 @@ ip_objs = extract_network_netname(unique_ips)
 for obj in ip_objs:
 	bgp_networks.append(obj.bgp_network)
 bgp_networks = set(bgp_networks)
-for ip in bgp_networks:
-	print(ip)
+
+for count, obj in enumerate(ip_objs):
+	if bgp_networks[count] == obj.bgp_network: print(f"{ip:<20} {ext_ips.bgp_network:<20} {ext_ips.isp_netname:<20}")
