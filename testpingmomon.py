ae6ea1998ac1e9fdedeaf9b115ef9e03e8af0077
KylePac <147895229+km-pac@users.noreply.github.com>
2024-05-19 11:02:23 +0800

    Minor 1

--- testpingmomon.py
+++ testpingmomon.py
@@ -27,7 +27,7 @@ def extract_network_netname(unique_ips):
 		except: continue
 
 		ip = ExtractedIP(parsed_ip, parsed_netname)
-		print(ip.bgp_network, ip.isp_netname)
+		return ip
 
 
 file_path = "clientips.txt"
@@ -39,4 +39,5 @@ with open(file_path, 'r') as file:
 os.system("clear")
 
 print(f"EXTRACT COUNT: {len(extracted_ips)} \nUNIQUE COUNT: {len(unique_ips)}")
-extract_network_netname(unique_ips)
+ips = extract_network_netname(unique_ips)
+print(ips)
