c81eb4bf28ab8deebc1d65190a17174b0a4b98b9
KylePac <147895229+km-pac@users.noreply.github.com>
2024-05-19 10:58:35 +0800

    Configured test file

--- testpingmomon.py
+++ testpingmomon.py
@@ -1,13 +1,34 @@
 import requests, os
 from tabulate import tabulate
 
-bgp_peer = []
-bgp_isp = []
+class ExtractedIP:
+	def	__init__(self, bgp_network, isp_netname):
+		self.bgp_network = bgp_network
+		self.isp_netname = isp_netname
 
-target_url = "https://bgp.he.net/ip/"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SM-G996U Build\\/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36',
-}
+	def pingnet(self):
+		os.system(f"fping -g {self.bgp_network}")
+
+def extract_network_netname(unique_ips):
+	target_url = "https://bgp.he.net/ip/"
+	headers = {
+    	'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SM-G996U Build\\/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36',
+		}
+
+	for count, ip in enumerate(unique_ips):
+		response = requests.get(target_url + ip, headers=headers)
+		data = response.text
+	
+		try: 
+			ips = [line for line in data.split('\n') if "/net/" in line][0]
+			netnames = [line for line in data.split('\n') if "netname:" in line or "NetName:" in line][0]	
+			parsed_ip = ips.strip().split("/net/")[1].split("\">")[0]
+			parsed_netname = netnames.split(":")[1].strip()
+		except: continue
+
+		ip = ExtractedIP(parsed_ip, parsed_netname)
+		print(ip)
+
 
 file_path = "clientips.txt"
 with open(file_path, 'r') as file: 
@@ -15,32 +36,7 @@ with open(file_path, 'r') as file:
 	extracted_ips = [line.strip() for line in lines]
 	unique_ips = set(extracted_ips)
 
-print(f"{unique_ips}\n")
 os.system("clear")
 
 print(f"EXTRACT COUNT: {len(extracted_ips)} \nUNIQUE COUNT: {len(unique_ips)}")
-for count, ip in enumerate(unique_ips):
-	response = requests.get(target_url + ip, headers=headers)
-	data = response.text
-	
-	try: 
-		ips = [line for line in data.split('\n') if "/net/" in line][0]
-		netnames = [line for line in data.split('\n') if "netname:" in line or "NetName:" in line][0]	
-		parsed_ip = ips.strip().split("/net/")[1].split("\">")[0]
-		parsed_netname = netnames.split(":")[1].strip()
-	except: continue
-
-	if count == 0: print(f"{'Index':<10} {'IP':<20} {'PARSED IP':<20} {'ISP':<20}")	
-	print(f"{count+1:<10} {ip:<20} {parsed_ip:<20} {parsed_netname:<20}")
-
-	bgp_peer.append(parsed_ip)
-	bgp_isp.append(parsed_netname)
-	
-parsed_bgp_peer = set(bgp_peer)
-print(f"BGP COUNT: {len(bgp_peer)} \nPARSED BGP COUNT: {len(parsed_bgp_peer)}")
-
-for count, ip in enumerate(bgp_peer):
-	print(ip, bgp_isp[count])
-print("\n")
-for count, ip in enumerate(bgp_peer):
-	print(ip)
+extract_network_netname(unique_ips)
