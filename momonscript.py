bfe4b2930b8818500776cb42b3371d4d460f429a
KylePac <147895229+km-pac@users.noreply.github.com>
2024-05-24 12:57:49 +0800

    Replaced files

--- momonscript.py
+++ momonscript.py
@@ -1,10 +1,6 @@
-import os
-from testpingmomon import extract_parse_clients
-from testpingmomon import extract_bgp_network
+from momonfunctions import *
 
 file_path = "clientips.txt"
 os.system("clear")
 extracted_ips, unique_ips = extract_parse_clients(file_path)
-
 extract_bgp_network(unique_ips)
-print("test")
