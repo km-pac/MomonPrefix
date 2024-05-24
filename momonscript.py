2c7e006d828c24dca376e00d6dade26af58afd45
KylePac <147895229+km-pac@users.noreply.github.com>
2024-05-24 13:05:38 +0800

    Updated extract bgp network function

--- momonscript.py
+++ momonscript.py
@@ -4,4 +4,5 @@ from momonfunctions import *
 file_path = "clientips.txt"
 os.system("clear")
 extracted_ips, unique_ips = extract_parse_clients(file_path)
-extract_bgp_network(unique_ips)
+bgp_networks = extract_bgp_network(unique_ips)
+print(bgp_networks)
